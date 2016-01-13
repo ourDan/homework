4489b1ca8a6d3fa04bc2c914388a0ee51d640a26
ourDan <17600858977@163.com>
2016-01-13 11:51:22 +0800

    昨天为什么没有发出去

--- myjs.js
+++ myjs.js
@@ -80,8 +80,6 @@ window.onload = function(){
     function sleep(){
         // 这个是得要 自己去把中间的函数绘制了
         getCenterFill("(255,255,255)",150,ctxCenter,300,300);
-        //openAndClose(300,300,2,850);
-        //openAndClose([[300,300],[300,5],[0.1,0.2]],2,300,[ctxBorderDom,ctxCenterDom]);//[[],[],[]]
         var dtd = $.Deferred();
         var wait = function(dtd){
             var taskOne = function(){
@@ -89,7 +87,6 @@ window.onload = function(){
 
                 setTimeout(function(){
                     dtd.resolve();
-                    //console.log("i++","end");
                 },1200) // 难道只能用这个函数 为什么我的动画进程 那就是说 说什么 只有在 动画完成的时候 ，还得 分别去 加载 针对动画类型的函数 难道不是一条线？
 
             };
@@ -110,31 +107,23 @@ window.onload = function(){
 
     // 第四个就是  惊讶.mov 
     function surprise(){  
-        
         getCenterFill("rgb(255,255,255)",150,ctxCenter,300,300); 
         getDoubleBorderCircle("rgb(255,255,255)",150,6,ctxBorder,300,300,1)
-
         var dtd = $.Deferred();
-
         var wait = function(dtd){
             var taskOne = function(){
-
                 easyOpenAndClose([[300,180],[300,180]],0,500,[ctxCenterDom]);
-
                 setTimeout(function(){
                     dtd.resolve();
                     //console.log("i++","end");
                 },1300) // 难道只能用这个函数 为什么我的动画进程 那就是说 说什么 只有在 动画完成的时候 ，还得 分别去 加载 针对动画类型的函数 难道不是一条线？
-
             };
             taskOne();
-        }
-        
+        };
+
         dtd.promise(wait);
         wait.done(function(){
-                //moreOrLess([[300,500],[300,500]],[ctxCenterDom,ctxBorderDom],1,900);
                 openAndClose([[300,300],[300,25],[0,1]],0,450,[ctxBorderDom,ctxCenterDom]);//[[],[],[]]
-                
                 })
             .fail(function(){
                     //getup();
@@ -152,14 +141,112 @@ window.onload = function(){
     //getUp();
     
     // 左右给个动作跟随   
-    followToBorder([40,0]);
+    //followToBorder([40,0]);
     
     // 睡眠动画
     //sleep(); 
     
     // 惊讶动画 
     //surprise();
- 
+    
+    
+    // 开始制作接受 分发 运行 
+    // 今天要完成的测试功能 就是 能够根据命令写动作 
+    //首先是websocket通信方面 kj
+    var socket = io.connect("http://192.168.1.48:9527/xiaoweiui");
+
+    socket.on("connect",function(){
+        console.log("socket is connected");
+    });
+
+    socket.on("message",function(message){
+        // 就是说 接受到了 信息 msg = {"name":"type(可能存在的各种值)","参数":"每个参数之间都是，分割"}
+        var messageObj = read(message);
+        //假如说 那个 正在跑一个 然后传命令过来突然让我停了 我也头疼现在怎么写
+        //根据传进来的参数再 可是我现在只有action  ；如果有了weather ；我在哪里判断会好一些 
+        var actionsObj = prepareDoAction(messageObj); //就是说 就是说什么呢 这里面先创建每个动作的对象 对象在动作结束后 自己写个回调去注销 
+
+        actionsObj.start();
+    }) ;
+
+    // 这个函数就是 分解 通信过来的参数 
+    function readAnd(msg){
+        if(msg["name"] == "action"){  //就是说 传入的就是个 action动作 接下来就得是 分解
+            var newAction = {
+                "actionName":msg["args"][0],
+                "actionArgsArr":msg["args"][1].spilt(","); 
+            }
+            return newAction
+        }
+        else if(msg["name"] == "weather"){  //针对传入的 message什么的
+            console.log("i am not write action ready");
+            //return xxxAction
+        }
+    }
+         
+    // 根据刚才 读取的 去 写动作  这个就是具体的动作
+    function  prepareDoAction(msgObj){
+        // 这个怎么设计我也没想好 但是 肯定
+        var newActObj = {};
+        switch(msgObj["actionArgsArr"][0]){  //去判断 动作类 的 哪一个动作在运行 
+            case "surprise": //就是说 说这个是 surprise动作 
+                // 对于surprise动作 根本就是 两个参数 
+                var callback1 = function(){
+                    return easyOpenAndClose([[300,180],[300,180]],0,500,[ctxCenterDom]);
+                }
+                var callback2 = function(){
+                    return openAndClose([[300,300],[300,25],[0,1]],0,450,[ctxBorderDom,ctxCenterDom]);
+                }
+                newActObj = new actionCreater(callback1,callback2 ) // 根据动作的参数 可以修改 
+                
+                break;
+        return  newActObj;       
+                
+        }
+    }
+
+
+
+
+    //用来建立 动作对象的 原型继承的 
+    function actionCreater(fun1,fun2){
+        this.callBackOne = fun1;
+        this.callBackTwo = fun2;
+    };
+
+    actionCreater.start = function(){ //  这么写是适合通用的功能 但是像那一种 比如  需要重写特例 
+        getCenterFill("rgb(255,255,255)",150,ctxCenter,300,300); 
+        getDoubleBorderCircle("rgb(255,255,255)",150,6,ctxBorder,300,300,1)
+        this.dtd = $.Deferred();
+        var wait = function(dtd){
+            var taskOne = function(){
+                //easyOpenAndClose([[300,180],[300,180]],0,500,[ctxCenterDom]);
+                this.callBackOne();
+                setTimeout(function(){
+                    dtd.resolve();
+                    //console.log("i++","end");
+                },1300) // 难道只能用这个函数 为什么我的动画进程 那就是说 说什么 只有在 动画完成的时候 ，还得 分别去 加载 针对动画类型的函数 难道不是一条线？
+            };
+            taskOne();
+        };
+
+        //dtd.promise(wait);
+        wait.done(function(){
+                //openAndClose([[300,300],[300,25],[0,1]],0,450,[ctxBorderDom,ctxCenterDom]);//[[],[],[]]
+                this.callBackTwo()
+                })
+            .fail(function(){
+                    //getup();
+                })    
+        wait(this.dtd);   
+    }
+    actionCreater.end = function(){
+        this.dtd.reject();
+    }
+    actionCreater.stop = function(){
+        // 这里就是 涉及 到 requestAnimationFrame 的重新写 
+    }
+
 }
 
 
